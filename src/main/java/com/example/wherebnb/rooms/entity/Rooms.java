09012d90a4e04c9b7a9634aea4382b76551f01ca
jeonga127 <j970924@gmail.com>
2023-05-13 12:21:19 +0900

    Test : security 테스트(1)

--- src/main/java/com/example/wherebnb/rooms/entity/Rooms.java
+++ src/main/java/com/example/wherebnb/rooms/entity/Rooms.java
@@ -1,4 +1,13 @@
 package com.example.wherebnb.rooms.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Rooms {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
---
+++ src/main/java/com/example/wherebnb/users/service/UserService.java
@@ -0,0 +1,104 @@
+package com.example.wherebnb.users.service;
+
+import com.example.wherebnb.global.dto.ResponseDto;
+import com.example.wherebnb.global.jwt.JwtUtil;
+import com.example.wherebnb.users.dto.UserInfoDto;
+import com.example.wherebnb.users.entity.Users;
+import com.example.wherebnb.users.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+
+    public ResponseDto kakaoLogin(String code, HttpServletResponse response) throws JsonProcessingException {
+        //1. "인가 코드"로 "액세스 토큰" 요청
+        String accessToken = getToken(code);
+
+        //2. 토큰으로 카카오 API 호출 : "액세스 토큰"으로 "카카오 사용자 정보" 가져오기
+        UserInfoDto userInfoDto = getUserInfo(accessToken);
+
+        //3. user repository에 user가 있는지 확인 / 없다면 넣음
+        if(!userRepository.existsByKakaoId(userInfoDto.getKakaoId()))
+            userRepository.save(new Users(userInfoDto));
+
+        //4. JWT 토큰 반환
+        String token = jwtUtil.createToken(userInfoDto.getUsername());
+        // response.addHeader("Authorization", token);
+        Cookie cookie = new Cookie("Authorization", token.substring(7));
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return ResponseDto.setSuccess("로그인 성공", userInfoDto.getUsername());
+    }
+    private String getToken(String code) throws JsonProcessingException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", "8047e89d739d21f2f220a5e8df94ddd0");
+        body.add("redirect_uri", "http://localhost:8080/user/login");
+        body.add("code", code);
+
+        // HTTP 요청 보내기
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(body, headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        // HTTP 응답 (JSON) -> 액세스 토큰 파싱
+        String responseBody = response.getBody();
+        log.info(responseBody);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        return jsonNode.get("access_token").asText();
+    }
+
+    // 토큰에서 사용자 정보 get
+    private UserInfoDto getUserInfo(String token) throws JsonProcessingException {
+        // HTTP Header 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HTTP 요청 보내기
+        HttpEntity<MultiValueMap<String, String>> UserInfoRequest = new HttpEntity<>(headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> response = rt.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST, UserInfoRequest, String.class);
+
+
+        String responseBody = response.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+        String username = jsonNode.get("properties").get("nickname").asText();
+        Long kakaoId = jsonNode.get("id").asLong();
+
+        return new UserInfoDto(username, kakaoId);
+    }
+}
