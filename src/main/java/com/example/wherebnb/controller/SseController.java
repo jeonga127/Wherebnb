e99f12a6239b2b888454afc21ca43a7d8a46c1ff
jeonga127 <j970924@gmail.com>
2023-05-15 14:14:25 +0900

    Feat : Sse Controller 추가

--- src/main/java/com/example/wherebnb/controller/SseController.java
+++ src/main/java/com/example/wherebnb/controller/SseController.java
@@ -1,4 +1,46 @@
 package com.example.wherebnb.controller;
 
+import com.example.wherebnb.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
 public class SseController {
+
+    public static Map<Long, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
+    private final JwtUtil jwtUtil;
+
+    @CrossOrigin
+    @GetMapping(value = "/sub", consumes = MediaType.ALL_VALUE)
+    public SseEmitter subscribe (@RequestParam("token") String token){
+
+        Long kakaoId = Long.valueOf(jwtUtil.getUserInfoFromToken(token));
+
+        SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
+        try{
+            sseEmitter.send(SseEmitter.event().name("connect"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sseEmitters.put(kakaoId,sseEmitter);
+
+        sseEmitter.onCompletion(()->sseEmitters.remove(kakaoId));
+        sseEmitter.onTimeout(()->sseEmitters.remove(kakaoId));
+        sseEmitter.onError((e)-> sseEmitters.remove(kakaoId));
+
+        return sseEmitter;
+    }
 }
