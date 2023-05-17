576f2df28ac65b66a3532b14bff61f5e5efe8700
jeonga127 <j970924@gmail.com>
2023-05-18 01:41:14 +0900

    Chore : 불필요한 코드 삭제

--- src/main/java/com/example/wherebnb/config/web/WebSecurityConfig.java
+++ src/main/java/com/example/wherebnb/config/web/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console())
+                //.requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
                 .and().ignoring().requestMatchers(PERMIT_URL_ARRAY);
     }
